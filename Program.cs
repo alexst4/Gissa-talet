f925151834f396b82be79d212d2841bafcf7b727
alexst4 <112472602+alexst4@users.noreply.github.com>
2022-09-06 13:31:44 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,29 @@
-﻿using System;
+Random randomerare = new Random();
+int slump_tal = randomerare.Next(1, 10);
 
-internal class Program
+Console.WriteLine("Gissa ett tal mellan 1 och 10!");
+string str = Console.ReadLine();
+int tal = Convert.ToInt32(str);
+
+while (tal < slump_tal)
+
 {
-    private static void Main(string[] args)
-    {
-        
-    }
+    Console.WriteLine("Större!");
+    str = Console.ReadLine();
+    tal = Convert.ToInt32(str);
+}
+
+while (tal > slump_tal)
+
+{
+    Console.WriteLine("Mindre!");
+    str = Console.ReadLine();
+    tal = Convert.ToInt32(str);
+}
+
+if (tal == slump_tal)
+
+{
+    Console.WriteLine("Grattis! Du gissade rätt!");
+    Console.ReadLine();
 }
